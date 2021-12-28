4c39c208051110ffe0f88b52858cc9368c5827f3
Леонов Кирилл Валентинович <18762488@sigma.sbrf.ru>
2021-12-28 07:41:34 +0700

    initial commit

---
+++ sse/src/test/java/edu/samples/SseApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
